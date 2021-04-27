ac8d7dba39ff5fd2f3603689e4056d1cdb65021a
KanwaljeetSinghKalra <kkalra@roostify.com>
2021-04-27 13:56:51 -0700

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import logo from "./../logo.svg";
 import "./../styles/App.css";
+import { Card, CardBody, CardHeader } from "reactstrap";
 import CreateLink from "./CreateLink";
 import LinkList from "./LinkList";
 
 function App() {
   return (
     <div className="App1">
-      <LinkList />
-      {`\n`}
-      <h3>Create Link Form</h3>
-      <CreateLink />
+      <h1>Fulll Stach React Apollo GraphQL Application</h1>
+
+      <Card className="mb-3">
+        <CardHeader>Create Link Form</CardHeader>
+        <CardBody>
+          <CreateLink />
+        </CardBody>
+      </Card>
+
+      <Card>
+        <CardHeader>Feed Links List</CardHeader>
+        <CardBody>
+          <LinkList />
+        </CardBody>
+      </Card>
     </div>
   );
 }
--- src/components/CreateLink.jsx
+++ src/components/CreateLink.jsx
@@ -1,5 +1,17 @@
 import React, { useState } from "react";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { useMutation, gql } from "@apollo/client";
+
+const CREATE_LINK_MUTATION = gql`
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`;
 
 const CreateLink = (props) => {
   const [formState, setFormState] = useState({
@@ -7,17 +19,29 @@ const CreateLink = (props) => {
     url: "",
   });
 
+  const [createLink] = useMutation(CREATE_LINK_MUTATION, {
+    variables: {
+      url: formState.url,
+      description: formState.description,
+    },
+  });
+
   const changeHandler = (field, value) =>
     setFormState({ ...formState, [field]: value });
 
   return (
-    <Form onSubmit={(e) => e.preventDefault()}>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        createLink();
+      }}
+    >
       <FormGroup>
         <Label for="description_input">Description</Label>
         <Input
           id="description_input"
           value={formState.description}
-          onChange={(val) => changeHandler("description", val)}
+          onChange={(e) => changeHandler("description", e.target.value)}
         />
       </FormGroup>
 
@@ -26,7 +50,7 @@ const CreateLink = (props) => {
         <Input
           id="url_input"
           value={formState.url}
-          onChange={(val) => changeHandler("url", val)}
+          onChange={(e) => changeHandler("url", e.target.value)}
         />
       </FormGroup>
 
